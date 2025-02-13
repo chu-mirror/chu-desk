f23cf5542b129340aa17b6334b6202c5243282b2
chu <maedaqu@gmail.com>
2025-02-13 21:44:02 +0800

    Minor fix

--- install/emacs.sh
+++ install/emacs.sh
@@ -5,9 +5,9 @@ which emacs 1>/dev/null && exit 0
 emacs_src_path=$HOME/reps/emacs
 
 [ -d $emacs_src_path ] || \
-  git clone https://git.savannah.gnu.org/git/emacs.git $emacs_src_path
+  git clone https://github.com/emacs-mirror/emacs.git $emacs_src_path
 
-cd $emacs_src_path
-./configure --with-modules && \
-  make && \
-  sudo make install
+[ -d $emacs_src_path ] && cd $emacs_src_path && \
+  ./configure --with-modules && \
+    make && \
+    sudo make install
