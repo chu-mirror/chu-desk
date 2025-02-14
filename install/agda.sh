d23c62ffb60b99481b761d27dc017e4a0a0ee2b0
chu <maedaqu@gmail.com>
2025-02-14 21:00:40 +0800

    Fix bug

--- install/agda.sh
+++ install/agda.sh
@@ -1,8 +1,4 @@
 #!/bin/sh -e
 
-chu_desk_add_dep(cabal-install)m4_dnl
-
 cabal update
 cabal install Agda
-cd "$REPOSITORY_HOME"
-git clone --recurse-submodules git@github.com:chu-mirror/agda.git "$REPOSITORY_HOME/agda"
