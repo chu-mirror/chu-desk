f3c6e0ada64cb0a0cf485876668846ed6b723dea
Chu <maedaqu@gmail.com>
2025-02-26 10:02:06 +0800

    Add vim-textobj-entire

--- install/vim-addons.sh
+++ install/vim-addons.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
-mkdir -p ~/.vim/pack/tpope/start
-cd ~/.vim/pack/tpope/start
-git clone https://tpope.io/vim/commentary.git
+cd ~/.vim/pack
+
+[ -d tpope ] || mkdir -p tpope/start
+cd tpope/start
+[ -d commentary ] || git clone https://tpope.io/vim/commentary.git
+
+cd ~/.vim/pack
+[ -d kana ] || mkdir -p kana/start
+cd kana/start
+[ -d vim-textobj-user ] || git clone https://github.com/kana/vim-textobj-user.git
+[ -d vim-textobj-entire ] || git clone https://github.com/kana/vim-textobj-entire.git
